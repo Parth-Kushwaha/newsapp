51d145b72769f5d7a9ced53fc907ea7ac47ab54d
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-13 01:53:17 +0530

    a

--- src/App.js
+++ src/App.js
@@ -19,6 +19,12 @@ export default class App extends Component {
         <Navbar/>
         <Routes>
           <Route exact path="/general" element={<News pageSize={9} country="in" category="general"/>}></Route>
+          <Route exact path="/business" element={<News key="business" pageSize={9} country="in" category="business"/>}></Route>
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={9} country="in" category="entertainment"/>}></Route>
+          <Route exact path="/health" element={<News key="healths" pageSize={9} country="in" category="health"/>}></Route>
+          <Route exact path="/science" element={<News key="science" pageSize={9} country="in" category="science"/>}></Route>
+          <Route exact path="/sports" element={<News key="sports" pageSize={9} country="in" category="sports"/>}></Route>
+          <Route exact path="/technology" element={<News key="technology" pageSize={9} country="in" category="technology"/>}></Route>
         </Routes>
         
         </Router>
