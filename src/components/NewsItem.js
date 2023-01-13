340a9e6d06fe2f5d9f2b0fd1ba6ea1aac50c9cb9
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-13 11:46:20 +0530

    a

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl, author, date}= this.props;
+    let {title, description, imageUrl, newsUrl, author, date, source}= this.props;
     return (
       <div>
         <div className="card">
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{source}</span>
             <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
