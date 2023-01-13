330e1d8a00ca594cdada996ce30f4d68393ec85a
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-13 12:10:36 +0530

    a

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export default class NewsItem extends Component {
     return (
       <div>
         <div className="card">
-            <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: "85%", zIndex: '1'}}>{source}</span>
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: "85%", zIndex: '1'}}>{source}</span>
             <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
