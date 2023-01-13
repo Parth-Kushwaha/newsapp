eaaf954425dff2d706920e62b23543ed3cbf82e6
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-13 11:33:12 +0530

    a

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl}= this.props;
+    let {title, description, imageUrl, newsUrl, author, date}= this.props;
     return (
       <div>
         <div className="card">
@@ -10,6 +10,7 @@ export default class NewsItem extends Component {
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
+                <p className="card-text">By: {!author? "Unknown": author} on {!date?"Unknown":new Date(date).toGMTString()}</p>
                 <a href={newsUrl} target="_blank"className="btn btn-sm btn-dark">READ MORE</a>
             </div>
         </div>
