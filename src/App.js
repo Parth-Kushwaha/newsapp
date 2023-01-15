9c0db1236bba126425c7883f21e08776abdddbe1
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-15 23:37:42 +0530

    c

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-
+import LoadingBar from 'react-top-loading-bar';
 import {
   BrowserRouter as Router,
   Routes, 
