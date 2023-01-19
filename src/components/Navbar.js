e152bf7ef1e0f163e7b1c00c25edb2306481f0ac
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-19 12:38:58 +0530

    sticky navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar=()=>{
     return (
-        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <nav className="navbar fixed-top navbar-expand-lg bg-dark navbar-dark">
           <div className="container-fluid">
             <Link className="navbar-brand"to="/general">Dekhle Bhai</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
