905bb6465ba90bc0b6ca56f0e22134e7a35d659e
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-19 12:29:06 +0530

    c

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 
-export default class Spinner extends Component {
-  render() {
+export default function Spinner=()=>{
     return (
       <div className="d-flex justify-content-center my-3">
         <div className="spinner-border text-dark" role="status"></div>
       </div>
     )
-  }
 }
