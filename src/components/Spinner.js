b375f84a0270e7d11e508285ed8afe5e6ab61eac
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-01-19 12:32:30 +0530

    spinner to function based

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 
-export default function Spinner=()=>{
+const Spinner=()=>{
     return (
       <div className="d-flex justify-content-center my-3">
         <div className="spinner-border text-dark" role="status"></div>
       </div>
     )
 }
+
+export default Spinner
